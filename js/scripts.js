2b24d6c20470a211cdc6e55714a74d13fc2d7065
Jason Awbrey and Neil Larion <jason.s.awbrey@gmail.com>
2016-01-21 11:58:41 -0800

    Add typeChcker, quoitentGreaterThanOne, and returnWholeQuotient

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
+var romanNums = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+
+
+
+var typeChecker = function(userNum){
+  if(typeof(userNum) !== "number"){
+    return false;
+  } else {
+    return true;
+  }
+};
+
+var quotientGreaterThanOne = function(userNum, divisor){
+  if((userNum/divisor) >= 1){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var returnWholeQuotient =  function(numerator,divisor){
+  return parseInt(numerator/divisor);
+};
+
+// var numChopper = function(integer){
+//
+//   for
+//
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,25 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
+describe("typeChecker",function(){
+  it("Will return false if not given a number",function(){
+    expect(typeChecker("bull")).to.equal(false);
+  });
+});
+
+describe("quotientGreaterThanOne", function(){
+  it("Will return true if quotient is greater than one", function(){
+    expect(quotientGreaterThanOne(14, 10)).to.equal(true);
+  });
+});
+
+describe("returnWholeQuotient", function(){
+  it("Will return whole number based on quotient", function(){
+    expect(returnWholeQuotient(14,10)).to.equal(1);
+  });
+});
+
+
+
+// describe("numChopper", function(){
+//   it("Will return an array of integers that correspond to given groupings", function(){
+//     expect(numChopper(14)).to.eql([4,0,1,0,0,0,0]);
 //   });
 // });
